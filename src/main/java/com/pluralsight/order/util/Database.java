d51a49ee4b708cfc490b3054b84f312b75d721c2
Freddy12321 <34176620+Freddy12321@users.noreply.github.com>
2020-04-04 17:07:13 +0100

    a

--- src/main/java/com/pluralsight/order/util/Database.java
+++ src/main/java/com/pluralsight/order/util/Database.java
@@ -5,6 +5,7 @@ import org.h2.tools.RunScript;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
 /**
@@ -18,7 +19,9 @@ public class Database {
      * Private constructor
      */
     private Database() { }
-
+    private String url = "jdbc:h2:mem:orders;DB_CLOSE_DELAY=-1";
+    private String user="sa";
+    private String password="";
     /**
      * Method that creates an instance of this class (if it's not created already)
      * @return Instance of the class
@@ -49,7 +52,7 @@ public class Database {
      * @throws SQLException In case of a database error
      */
     public Connection getConnection() throws SQLException {
-        Connection connection = null;
+        Connection connection = DriverManager.getConnection(url, user, password);
 
         if(!isInitialized && connection != null) {
             initializeDatabase(connection);
